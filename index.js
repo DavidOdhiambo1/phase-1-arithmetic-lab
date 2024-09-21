80a5b4602d15fa535451fc699be658adab73522a
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-09-21 07:05:07 +0300

    Math.random,.max,.round

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+const random = Math.round(Math.random()*10)
+
+const num3 = 14
+const num4 = 5
+const mod = num3%num4
+
+const max = Math.max(1,20,15,5)
